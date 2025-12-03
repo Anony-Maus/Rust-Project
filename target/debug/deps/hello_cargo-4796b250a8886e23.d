29935d1774bc2b19c5d4abaed6895094403d8253
Anony-Maus <goldsten591@gmail.com>
2025-12-03 09:56:29 -0800

    Initial commit

---
+++ target/debug/deps/hello_cargo-4796b250a8886e23.d
@@ -0,0 +1,5 @@
+/Users/austin/Documents/Code/Rust/Rust-Project/hello_cargo/target/debug/deps/hello_cargo-4796b250a8886e23.d: src/main.rs
+
+/Users/austin/Documents/Code/Rust/Rust-Project/hello_cargo/target/debug/deps/libhello_cargo-4796b250a8886e23.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/hello_cargo-87b39c049b5f344c.d
@@ -0,0 +1,5 @@
+/Users/austin/Documents/Code/Rust/Rust-Project/hello_cargo/target/debug/deps/hello_cargo-87b39c049b5f344c.d: src/main.rs
+
+/Users/austin/Documents/Code/Rust/Rust-Project/hello_cargo/target/debug/deps/libhello_cargo-87b39c049b5f344c.rmeta: src/main.rs
+
+src/main.rs:
