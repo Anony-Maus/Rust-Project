bb8953bd0acf29d8b9420dbab3724af8704bf292
Austin Oglesby <goldsten591@gmail.com>
2025-12-06 17:10:04 -0800

    Initial linux commit.

---
+++ target/debug/deps/hello_cargo-687e0f446784b5cc.d
@@ -0,0 +1,5 @@
+/home/anon/Documents/Coding/Rust/Rust-Project/target/debug/deps/hello_cargo-687e0f446784b5cc.d: src/main.rs
+
+/home/anon/Documents/Coding/Rust/Rust-Project/target/debug/deps/libhello_cargo-687e0f446784b5cc.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/hello_cargo-949b5935b01f26fa.d
@@ -0,0 +1,5 @@
+/home/anon/Documents/Coding/Rust/Rust-Project/target/debug/deps/hello_cargo-949b5935b01f26fa.d: src/main.rs
+
+/home/anon/Documents/Coding/Rust/Rust-Project/target/debug/deps/libhello_cargo-949b5935b01f26fa.rmeta: src/main.rs
+
+src/main.rs:
